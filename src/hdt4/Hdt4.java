251c754ff1ddb782e734d8ba57595f18d76e3701
erickDM <erickdemata@gmail.com>
2014-08-07 14:22:44 -0600

    se agregaron clases de List

--- src/hdt4/Hdt4.java
+++ src/hdt4/Hdt4.java
@@ -7,6 +7,12 @@
 
 package hdt4;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Isa
@@ -18,6 +24,86 @@ public class Hdt4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //Stack<String> miStack = new StackVector<String>(); 
+        StackFactory factor = new StackFactory();
+        Stack<String> miStack = factor.getStack(parametro()); 
+        
+        
+        int numero;
+        double n1;
+        double n2;
+        String sCadenaSinBlancos = "";
+        String opcion;
+        // TODO code application logic here
+        //Detección del texto en el que se encuentra la operación
+        File f = new File( "src/texto.txt" );
+        
+        BufferedReader entrada;
+     
+        try {
+       
+        entrada = new BufferedReader( new FileReader( f ) );
+        String linea;
+        
+        while(entrada.ready()){
+            //Detecta lo que se encuentra en la linea leída del archivo.txt
+            linea = entrada.readLine();
+            System.out.println(linea);
+            linea = linea.substring(0);
+            System.out.println(linea);
+            numero = linea.length(); 
+            //Imprime lo que detecta para verificación
+            System.out.println(numero); 
+            for (int x=0; x < linea.length(); x++) {
+                if (linea.charAt(x) != ' '){
+                    //Guarda en una cadena la linea del calculo sin espacios
+                    sCadenaSinBlancos += linea.charAt(x);
+                }
+            }
+            //Imprime la linea sin espacios para verificación
+            System.out.println(sCadenaSinBlancos);
+            
+            //Evalúa lo que se encuentra dentro de la linea sin espacios hasta que se haya completado el largo
+            for(int x=0; x<sCadenaSinBlancos.length(); x++){
+                String dato =  Character.toString(sCadenaSinBlancos.charAt(x));
+                //Si el caracter no es una operación se guarda el dato
+                if( (dato.equals("+") || dato.equals("-") || dato.equals("*") || dato.equals("/")) != true){
+                    miStack.push(dato);
+                }
+                //Si el caracter es una suma saca de la pila los dos ultimos ingresado y realiza operacion
+                if(Character.toString(sCadenaSinBlancos.charAt(x)).equals("+")){
+                    n2 = Double.parseDouble(miStack.pop());
+                    n1 = Double.parseDouble(miStack.pop());
+                    miStack.push(Double.toString(n1 + n2));
+                }
+                //Si el caracter es una resta saca de la pila los dos ultimos ingresado y realiza operacion
+                if(Character.toString(sCadenaSinBlancos.charAt(x)).equals("-")){
+                    n2 = Double.parseDouble(miStack.pop());
+                    n1 = Double.parseDouble(miStack.pop());
+                    miStack.push(Double.toString(n1 - n2));
+                }
+                //Si el caracter es una multiplicacion saca de la pila los dos ultimos ingresado y realiza operacion
+                if(Character.toString(sCadenaSinBlancos.charAt(x)).equals("*")){
+                    n2 = Double.parseDouble(miStack.pop());
+                    n1 = Double.parseDouble(miStack.pop());
+                    miStack.push(Double.toString(n1 * n2));
+                }
+                //Si el caracter es una division saca de la pila los dos ultimos ingresado y realiza operacion
+                if(Character.toString(sCadenaSinBlancos.charAt(x)).equals("/")){
+                    n2 = Double.parseDouble(miStack.pop());
+                    n1 = Double.parseDouble(miStack.pop());
+                    miStack.push(Double.toString(n1 / n2));
+                }
+                
+            }
+            //Impresión de resultado
+            System.out.println(miStack.pop());
+            
+        }
+        }catch (IOException e) {
+        e.printStackTrace();
+        }
+        }
     }
     
 }
